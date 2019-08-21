f3f34cc9585300ce555057721b97355885d83a67
APCS2nd <APCS2nd@MHS-LT9-10>
2019-08-21 12:17:47 -0700

    Complete HEllo World Project

---
+++ Hello World Java Intro/src/HelloWorld.java
@@ -0,0 +1,12 @@
+//Rion Leung 
+// 08/16/2019
+//Prints the Message that introduces me to Java
+// Intro Test My ability to use Java
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! ...RkionL");
+	}
+
+}
