0191b5799ffc8945a39a4f0af9bb3c06f06e63ab
APCS2nd <APCS2nd@MHS-LT9-10>
2019-09-04 12:03:47 -0700

    Trying to get the sync to work

---
+++ Calculate Library/Calculate.java
@@ -0,0 +1,33 @@
+/*Rion Leung
+*August 29 2019
+*Period 4
+*Group of methods that perform math calculations ( a library)
+*/
+
+public class Calculate {
+	//A call to square returns the square of the value passed.
+	//It accepts and returns an integer.
+	public static int square (int number) {
+		int answer = 0;
+		answer = number * number;
+		return answer;
+		
+	}
+	//A call to cube returns the cube of the value passed.
+		//It accepts and returns an integer.
+		public static int cube(int number) {
+			int answer = 0;
+			answer = number * number * number;
+			return answer;
+			
+		}
+		//A call to average returns the average of the value passed.
+		//It accepts and returns an integer.
+		public static double average(double number1, double number2) {
+			int answer = 0;
+			answer = number * number;
+			return answer;
+			
+		}
+	
+}
--- Hello World Java Intro/src/HelloWorld.java
+++ Hello World Java Intro/src/HelloWorld.java
@@ -3,10 +3,24 @@
 //Prints the Message that introduces me to Java
 // Intro Test My ability to use Java
 
-public class HelloWorld {
+public class Pikachu {
+
 
 	public static void main(String[] args) {
-		System.out.println("Hello World! ...RkionL");
+		printPikachu(); 
+		hello();
+		drawBox();
+		drawBox();
+		drawBox();
 	}
-
+	public static void drawBox() {
+		printPlusMinusRow();
+		print2VertLinesRow();
+		print2VertLinesRow();
+		printPlusMinusRow();
+		System.out.println(); 
+	
+			
+		}
+	
 }
